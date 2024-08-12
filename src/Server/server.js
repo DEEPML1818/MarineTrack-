f1a8f8d5ad1844d0096b3a206daa037d93962a57
DEEPML1818 <98756231+DEEPML1818@users.noreply.github.com>
2024-08-12 19:31:05 +0800

    Update server.js

--- src/Server/server.js
+++ src/Server/server.js
@@ -5,7 +5,7 @@ const path = require('path');
  
 const app = express();
 const dataFilePath = path.join(__dirname, 'tideData.json');
-
+ 
 const lat = 5.2831;
 const lon = 115.2309;
 const apiKey = '995e1ac7-9933-4100-a1cc-d49eb6938a88';
